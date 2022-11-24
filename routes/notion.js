381e9192380bfe09df6b93f169d46c92fddfa5cc
HwarrMaster <hwarr.master@gmail.com>
2022-11-25 01:29:08 +0900

    axios error catch add

--- routes/notion.js
+++ routes/notion.js
@@ -1,60 +1,68 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const crypto = require('crypto')
+const crypto = require("crypto");
 
 router.get("/", async (req, res, next) => {
-  console.log('/notion 접속');
-  
-  const NOTION_ID = "fe39b1a16a6743689b166fc835fe0034";
-  const URL = `https://api.notion.com/v1/databases/${NOTION_ID}/query`;
+    console.log("/notion 접속");
 
-  const result = await axios.post(
-    URL,
-    {},
-    {
-      headers: {
-        Authorization:
-          "Bearer secret_B7VxSnKEc3fVFNryVxpKMcLcMLAcmUogVzcPGewFoP9",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Notion-Version": "2022-06-28",
-      },
-    }
-  );
-  
-  console.log('result', result);
+    const NOTION_ID = "fe39b1a16a6743689b166fc835fe0034";
+    const URL = `https://api.notion.com/v1/databases/${NOTION_ID}/query`;
 
-  res.status(200);
-  res.json(result.data);
+    const result = await axios
+        .post(
+            URL,
+            {},
+            {
+                headers: {
+                    Authorization:
+                        "Bearer secret_B7VxSnKEc3fVFNryVxpKMcLcMLAcmUogVzcPGewFoP9",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Notion-Version": "2022-06-28",
+                },
+            }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+
+    console.log("result", result);
+
+    res.status(200);
+    res.json(result.data);
 });
 
 router.post("/", async (req, res, next) => {
-  const NOTION_ID = "fe39b1a16a6743689b166fc835fe0034";
-  const URL = `https://api.notion.com/v1/pages`;
+    const NOTION_ID = "fe39b1a16a6743689b166fc835fe0034";
+    const URL = `https://api.notion.com/v1/pages`;
 
-  const number = parseInt(crypto.randomBytes(2).toString('hex'), 16);
+    const number = parseInt(crypto.randomBytes(2).toString("hex"), 16);
 
-  const body = {
-    parent: { database_id: NOTION_ID },
-    properties: {
-      uuid: {
-        number
-      },
-      ...req.body,
-    },
-  };
+    const body = {
+        parent: { database_id: NOTION_ID },
+        properties: {
+            uuid: {
+                number,
+            },
+            ...req.body,
+        },
+    };
 
-  const result = await axios.post(URL, body, {
-    headers: {
-      Authorization:
-        "Bearer secret_B7VxSnKEc3fVFNryVxpKMcLcMLAcmUogVzcPGewFoP9",
-        "Content-Type": "application/json",
-        "Notion-Version": "2022-06-28",
-    },
-  });
+    const result = await axios
+        .post(URL, body, {
+            headers: {
+                Authorization:
+                    "Bearer secret_B7VxSnKEc3fVFNryVxpKMcLcMLAcmUogVzcPGewFoP9",
+                "Content-Type": "application/json",
+                "Notion-Version": "2022-06-28",
+            },
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 
-  res.status(200);
-  res.json(result.data);
+    res.status(200);
+    res.json(result.data);
 });
 
 module.exports = router;
